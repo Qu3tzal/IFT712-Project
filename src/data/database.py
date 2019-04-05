f9799613969e0628eb6be12c0e5b56853f233ebb
Maxime Alvarez <maxime.alvarez@sfr.fr>
2019-04-04 21:29:19 -0400

    Created an empy Database class

---
+++ src/data/database.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import dataset
+
+
+class Database:
+    """ This class facilitates the manipulation of the database. """
+
+    def __init__(self):
+        """ Constructor.
+        """
+        pass
+
+    def get_train_dataset():
+        """ Returns the training dataset. """
+        return Dataset([], [])
+
+    def get_test_dataset():
+        """ Returns the test dataset. """
+        return Dataset([], [])
