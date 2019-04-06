facc556a01547700c3c19b9810479bcbdd75bed5
Maxime Alvarez <maxime.alvarez@sfr.fr>
2019-04-06 11:41:42 -0400

    Simple database loader

--- src/data/database.py
+++ src/data/database.py
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 -*-
+import os
 import dataset
+import pandas as pd
 
 
 class Database:
     """ This class facilitates the manipulation of the database. """
 
-    def __init__(self):
+    def __init__(self, path):
         """ Constructor.
+
+            Args:
+                path the path to the directory of the database
         """
-        pass
+        self.directory = path
+
+    def load(self):
+        """ Loads the content of the database in memory. """
+        train_dataframe = pd.read_csv(os.path.join(path, 'raw/train.csv'))
+        test_dataframe = pd.read_csv(os.path.join(path, 'raw/test.csv'))
+
+        self.train_dataset = Dataset(test_dataframe.drop('species', axis=1), test_dataframe['species'])
+        self.test_dataset = Dataset(test_dataframe.values)
 
     def get_train_dataset():
         """ Returns the training dataset. """
-        return Dataset([], [])
+        return self.train_dataset
 
     def get_test_dataset():
         """ Returns the test dataset. """
-        return Dataset([], [])
+        return self.test_dataset
