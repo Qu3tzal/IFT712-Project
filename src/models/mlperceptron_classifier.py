53220148d184359de1a6cb25976198c0b3168d23
Maxime Alvarez <maxime.alvarez@sfr.fr>
2019-04-08 23:52:51 -0400

    Created the MLPerceptronClassifier class (#3)

---
+++ src/models/mlperceptron_classifier.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import models.classifier
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+
+
+class MLPerceptronClassifier(models.classifier.Classifier):
+    """ This class implements the multilayer perceptron classifier. """
+
+    def __init__(self):
+        """ Constructor.
+
+            Arg:
+                name the name of the classifier
+        """
+        super().__init__("MLPerceptronClassifier")
+        self.mlp = GridSearchCV(
+                        MLPClassifier(
+                            solver='sgd',
+                            momentum=0.9,
+                            activation='tanh',
+                            hidden_layer_sizes=(99,), # We have 99 classes.
+                            learning_rate='adaptive', # Makes the learning rate smaller when the loss cease to decrease.
+                            max_iter=5000 # Make sure we have enough time to converge.
+                        ),
+                        {"alpha": 10.0 ** -np.arange(1, 7)},
+                        cv=10
+                    )
+
+
+    def train(self, inputs, targets):
+        """ Trains the model on the given dataset.
+
+            Arg:
+                inputs the inputs
+                targets the targets
+        """
+        self.mlp.fit(inputs, targets)
+
+    def predict(self, dataset):
+        """ Predicts the dataset.
+
+            Arg:
+                dataset the inputs to predict
+
+            Returns: the prediction of all inputs
+        """
+        return self.mlp.predict(dataset)
+
+    def score(self, inputs, targets):
+        """ Computes the accuracy on the given dataset.
+
+            Arg:
+                inputs the inputs
+                targets the targets
+
+            Returns: the accuracy
+        """
+        return self.mlp.score(inputs, targets)
